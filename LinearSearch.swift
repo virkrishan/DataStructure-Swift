8bdf495ef09118c97627b107e947cb005b9b8053
virkrishan <mailkrishanvir@gmail.com>
2019-09-20 12:06:09 +0530

    temp added

--- LinearSearch.swift
+++ LinearSearch.swift
@@ -8,19 +8,17 @@
 
 import Foundation
 
-
 let array = [1,5,36,65,56,556,6,45,45,545,5,54454,4353,3324,4,]
 
 func linearSearch(array: [Int] , searchValue: Int) -> Int {
-    
     for data in array{
         if data == searchValue{
-            print("value Found \(searchValue)")
+            print(" search value Found \(searchValue)")
             return searchValue
         }else {
         }
     }
-    print("value not found")
+    print("search value not found")
     return 0
 }
 print("linear search \(linearSearch(array: array, searchValue: 45))")
